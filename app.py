faaf6c04ddc07a96dff4663c10de97df0df1d14e
SHARAT1535 <152896503+SHARAT1535@users.noreply.github.com>
2025-04-13 22:44:05 +0530

    connected to backend

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import base64
+import json
+from dotenv import load_dotenv
 import requests
 import time
 from threading import Thread
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from firebase_admin import auth 
+
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Needed for sessions & flash messages
+app.secret_key = 'your_secret_key'  
 
-# Initialize Firebase Admin SDK
-#cred = credentials.Certificate('iseconnect-ad8b0-firebase-adminsdk-fbsvc-e092490c1a.json')
-#firebase_admin.initialize_app(cred)
-#db = firestore.client()
+load_dotenv()
+
+encoded_key = os.getenv("FIREBASE_SERVICE_ACCOUNT_BASE64")
+
+# decode the stringand convert to python diction
+if encoded_key:
+    decoded_bytes = base64.b64decode(encoded_key)
+    service_account_info = json.loads(decoded_bytes)
+
+    # Initialize Firebase
+    cred = credentials.Certificate(service_account_info)
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+else:
+    raise ValueError("FIREBASE_SERVICE_ACCOUNT_BASE64 not found in .env file")
 
 @app.route('/')
 def hello():
@@ -23,6 +40,41 @@ def hello():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
+
+@app.route('/about_ise', methods=['GET', 'POST'])
+def about_ise():
+    return render_template('about_ise.html')
+
+
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        if password != confirm_password:
+            flash("Passwords do not match!")
+            return redirect(url_for('signup'))
+
+        try:
+            user = auth.create_user(
+                email=email,
+                password=password
+            )
+            flash("User created successfully!", "success")
+            return redirect(url_for('login')) 
+
+        except Exception as e:
+            flash(f"Error creating user: {str(e)}", "error")
+            return redirect(url_for('signup'))
+
+    return render_template('signup.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
